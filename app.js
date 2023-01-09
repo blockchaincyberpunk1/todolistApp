9d08cac6e73b37f69d6bcf03d36e81246ff107e8
Sheneeza Volcov <svolcov.blockchaindeveloper@gmail.com>
2023-01-09 05:14:23 -0500

    Add gitignore, procfile and update ports

--- app.js
+++ app.js
@@ -242,6 +242,11 @@ app.get("/about", function (req, res) {
   res.render("about");
 }); */
 
-app.listen(4000, function () {
-  console.log("Server started on port 4000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 4000;
+}
+
+app.listen(port, function () {
+  console.log("Server has started successfully.");
 });
